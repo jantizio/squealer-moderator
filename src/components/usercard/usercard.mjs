5fe98bd45b7d214abaed8dc1cd62b08b34b97697
jantizio <emanuele0246@gmail.com>
2023-11-11 10:29:19 +0100

    fix: db data update

--- src/components/usercard/usercard.mjs
+++ src/components/usercard/usercard.mjs
@@ -22,10 +22,19 @@ class Usercard extends HTMLElement {
     ).textContent = `${this._user.firstname} ${this._user.lastname}`;
     shadow.querySelector('.user-card__email').textContent = this._user.email;
     shadow.querySelector('.user-card__type').textContent = this._user.type;
-    shadow.querySelector('.user-card__smm').textContent = this._user.SMM;
-    // TODO: to be fixed, these are not primitive values
-    shadow.querySelector('.user-card__subscriptions').textContent =
-      this._user.subscriptions.join(', ');
+
+    if (this._user.SMM)
+      shadow.querySelector('.user-card__smm').textContent = this._user.SMM;
+    else shadow.querySelector('.user-card__smm').remove();
+
+    if (this._user.subscriptions.length > 0)
+      shadow.querySelector('.user-card__subscriptions').textContent =
+        this._user.subscriptions.join(', ');
+    else shadow.querySelector('.user-card__subscriptions').remove();
+
+    shadow.querySelector('#giorno').value = this._user.quota.maxD;
+    shadow.querySelector('#settimana').value = this._user.quota.maxW;
+    shadow.querySelector('#mese').value = this._user.quota.maxM;
   }
 
   get user() {
