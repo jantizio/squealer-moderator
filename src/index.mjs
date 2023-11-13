9439c2c3921a1b0d3f68c3a768f7df5f9aed9a5d
jantizio <emanuele0246@gmail.com>
2023-11-13 15:56:57 +0100

    fix: modules import

--- src/index.mjs
+++
@@ -1,9 +0,0 @@
-import {} from './components/example/example.mjs';
-import {} from './components/navbar/navbar.mjs';
-import {} from './components/user/user.mjs';
-import {} from './components/usercard/usercard.mjs';
-
-import {} from './components/squeal/squeal.mjs';
-// import {} from './components/squealcard/squealcard.mjs';
-
-import {} from './components/channel/channel.mjs';
