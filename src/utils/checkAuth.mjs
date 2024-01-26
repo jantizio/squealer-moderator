002e65ab9eb7b00ab984ec95c720d531bad16f46
jantizio <emanuele0246@gmail.com>
2024-01-26 21:06:05 +0100

    feat: block non authorized users

--- src/utils/checkAuth.mjs
+++ src/utils/checkAuth.mjs
@@ -1,5 +1,14 @@
+import { getMe } from "../api/users.mjs";
+
 // Check if browser has any cookies
 if (Cookies.get("logged_in") !== "true") {
     // If no cookies, redirect to login page
     window.location.href = "/login";
+}
+
+// if i am logged i need to check if the user is moderator
+const currentUser = await getMe()
+if(currentUser.type !== "moderator") {
+    // if not moderator redirect to home page
+    window.location.href = "/";
 }
